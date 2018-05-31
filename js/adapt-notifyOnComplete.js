2b4f670826bc9ea01ddc17f2f4c264a8a3b5e0c0
Paul Steven <paul@mediakitchen.co.uk>
2018-05-31 11:48:15 +0100

    Update adapt-notifyOnComplete.js
    Removed redundant multiple blank lines

--- js/adapt-notifyOnComplete.js
+++ js/adapt-notifyOnComplete.js
@@ -4,12 +4,13 @@ define([
 ], function(Adapt, Backbone) {
 
     function onDataReady() {
+
         var isEnabledOnCourse = isNotifyEnabled(Adapt.course);
-        if (isEnabledOnCourse) {           
+        if (isEnabledOnCourse) {
             Adapt.listenTo(Adapt.course, "change:_isComplete", onCourseComplete);
-        }     
-        
-         _.each(Adapt.contentObjects.models, function(model) {
+        }
+
+        _.each(Adapt.contentObjects.models, function(model) {
             if (isNotifyEnabled(model)) {
                 Adapt.listenTo(model, "change:_isComplete", onContentObjectComplete);
             }
@@ -19,38 +20,36 @@ define([
     function isNotifyEnabled(model) {
         return model.get("_notifyOnComplete") && model.get("_notifyOnComplete")._isEnabled;
     }
-    
+
     function onContentObjectComplete(pageModel) {
+
         if (Adapt.course.get("_isComplete")) {
             return;
         }
 
         var notifyConfig = pageModel.get("_notifyOnComplete");
+
         Adapt.trigger("notify:push", {
             title: notifyConfig.title,
             body: notifyConfig.body,
             _timeout: 5000,
             _callbackEvent: "navigation:backButton"
         });
-    }
- 
 
-    function onCourseComplete(courseModel) {     
-               
-        
-        
+    }
+
+    function onCourseComplete(courseModel) {
+
         var assessment = courseModel.get("_assessment");
 
         if (assessment && assessment._requireAssessmentPassed && !courseModel.get("_isAssessmentPassed")) return;
 
-        var notifyConfig = courseModel.get("_notifyOnComplete");   
-     
+        var notifyConfig = courseModel.get("_notifyOnComplete");
+
         Adapt.trigger("notify:popup", {
             title: notifyConfig.title,
-            body: notifyConfig.body           
+            body: notifyConfig.body
         });
- 
-              
     }
 
     Adapt.once("app:dataLoaded", onDataReady);
